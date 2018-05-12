9d8d10e2341d537d6bdb606c3b8836ab90964bbf
myao04 <web.info04s@gmail.com>
2018-05-12 11:45:56 +0000

    init Rails Project tasklist add

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511120753) do
+ActiveRecord::Schema.define(version: 20180512105234) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
