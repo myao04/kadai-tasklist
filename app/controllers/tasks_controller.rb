07b26e9170e210ba854fed5e37a57cce66b028bc
myao04 <web.info04s@gmail.com>
2018-05-18 14:51:58 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = Task.all.page(params[:page])
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
     if @task.save
       flash[:success] = 'Taskが正常に追加されました'
@@ -47,7 +48,7 @@ class TasksController < ApplicationController
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
-end
+
 
   private
   
@@ -58,3 +59,4 @@ end
   def task_params
     params.require(:task).permit(:content, :status)
   end
+end
