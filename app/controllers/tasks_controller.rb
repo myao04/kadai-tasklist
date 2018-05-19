f582f767ff58c00715b52e9a8c2913fa6272df3a
myao04 <web.info04s@gmail.com>
2018-05-19 12:41:57 +0000

    kadai3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:create, :edit, :update, :destroy]
+  
   def index
-      @tasks = Task.all.page(params[:page])
+    @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -58,5 +60,12 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_path
+    end
   end
 end
